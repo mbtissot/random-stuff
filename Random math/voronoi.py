2ab1bb027712409187c42b114eb4cc91e6faa6e9
Matheus <matheus.btissot@gmail.com>
2022-12-31 16:52:47 -0300

    Added PPM Voronoi

--- Random math/voronoi.py
+++ Random math/voronoi.py
@@ -2,69 +2,100 @@ import numpy
 from PIL import Image
 import random
 
-HEIGHT, WIDTH = 1025, 1025
-WHITE = [255, 255, 255]
-BLACK = [0,     0,   0]
+HEIGHT, WIDTH = 1024, 1024
+RED =      '255\t0\t0'
+GREEN =    '0\t255\t0'
+BLUE =     '0\t0\t255'
+YELLOW =   '255\t255\t0'
+WHITE =    '255\t255\t255'
+BLACK =    '0\t0\t0'
 
-SEED_NUM = 16
 
-#colors = [[random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)] for i in range(SEED_NUM)]
-
-global grid
-grid = numpy.zeros((HEIGHT, WIDTH, 3), dtype=numpy.uint8)
-
-#for i in range(SEED_NUM):
-#	seeds.append((random.randint(0, HEIGHT-1), random.randint(0, WIDTH-1)))
+def distance(x1, x2, y1, y2, norm):
+	return ((abs(x1-x2))**norm + (abs(y1-y2))**norm)**(1/norm)
 
 def fillseeds(seeds, grid):
-	for i in range(SEED_NUM):
-		grid[seeds[i][0], seeds[i][1]] = [255, 255, 255]
-	return renderCircleSeed(seeds, grid, 10)
+	print("Neighbours just called fillSeeds")
+	for i in range(len(seeds)):
+		grid[seeds[i][0]][seeds[i][1]] = BLACK
+	return renderCircleSeed(seeds, grid, 7)
 
 def renderCircleSeed(seeds, grid, radius):
+	print("fillSeeds just called renderCircleSeed")
 	for seed in seeds:
 		for i in range(seed[0]-radius, seed[0]+radius):
 			for j in range(seed[1]-radius, seed[1]+radius):
-				distSquare = (i-seed[0])**2 + (j-seed[1])**2
-				if distSquare<=radius*radius:
-					grid[i, j] = WHITE
-	return grid
+				if (seed[0]-radius<0 or seed[0]+radius>HEIGHT or seed[1]-radius<0 or seed[1]+radius>WIDTH):
+					continue
+				#dist = (i-seed[0])**2 + (j-seed[1])**2
+				dist = distance(i, seed[0], j, seed[1], 1.8)
+				if dist<=radius:
+					grid[i][j] = BLACK
+	return writeToPPM(grid)
 
 def defineNeighbours(seeds, grid, colors):
-	for i in range(WIDTH):
-		for j in range(HEIGHT):
+	print("Define Neighbours")
+	for i in range(HEIGHT):
+		for j in range(WIDTH):
 			listDistances = []
 			for seed in range(len(seeds)):
-				listDistances.append((i-seeds[seed][0])**2 + (j-seeds[seed][1])**2)
+				listDistances.append(distance(i, seeds[seed][0], j, seeds[seed][1], 2))
 				index = listDistances.index(min(listDistances))
-				grid[i, j] = colors[index]
+				grid[i][j] = colors[index]
 	return fillseeds(seeds, grid)
 
-#colors = []
+def randomColor():
+	cor = str(random.randint(0, 255))+'\t'+str(random.randint(0, 255))+'\t'+str(random.randint(0, 255))
+	return cor
 
-#for i in range(0, 1025, 256):
-#	for j in range(0, 1025, 256):
-#		seeds.append((i, j))
-#		colors.append([255, 255, 255])
+def writeToPPM(grid):
+	print("Started writing to file")
+	with open('output.ppm', 'w') as f:
+		f.write("P3\n")
+		f.write(str(WIDTH) + ' ' + str(HEIGHT) + "\n")
+		f.write('255\n') # max value
+		k=0
+		for i in range(HEIGHT):
+			for j in range(WIDTH):
+				f.write(str(grid[i][j]))
+				f.write('\n')
+	print("Done")
 
-#print(len(seeds))
 
-#colors[12] = [105, 105, 105]
 
-def config(configuration: str, configtype=None):
+def config(configuration: str, configtype=None, SEED_NUM=0):
 	if configuration=="random":
-		colors = [[random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)] for i in range(SEED_NUM)]
+		grid = [[0 for j in range(WIDTH)] for i in range(HEIGHT)]
+		colors = [randomColor() for i in range(SEED_NUM)]
 		seeds = [(random.randint(0, HEIGHT-1), random.randint(0, WIDTH-1)) for i in range(SEED_NUM)]
-		print('vim aqui')
-		Image.fromarray(defineNeighbours(seeds, grid, colors)).show()
+		defineNeighbours(seeds, grid, colors)
 	elif configuration=="wigner-seitz":
 		if configtype=="square":
 			seeds = [(i, j) for i in range(0, 1025, 256) for j in range(0, 1025, 256)]
-			colors = [[255, 255, 255] for i in range(len(seeds))]
-			colors[12] = [105, 105, 105]
+			colors = ['255\t255\t255' for i in range(len(seeds))]
+			colors[12] = '105\t105\t105'
+			SEED_NUM = 26
+			grid = [[0 for j in range(1025)] for i in range(1025)]
+			defineNeighbours(seeds, grid, colors)
 		elif configtype=="hexagonal":
 			print('cai aqui')
 			#to be done
 			return
+#print(grid)
+#config("wigner-seitz", "square")
+config("random", SEED_NUM=16)
 
-config("random")
+
+
+'''height = 10
+width = 4
+abc = [[0 for i in range(width)] for k in range(height)]
+print(abc)
+
+k=0
+for i in range(height):
+	for j in range(width):
+		abc[i][j] = k
+		k = k+1
+print(abc)
+'''
